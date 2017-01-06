cd63d5473019ffa184de85da850fe338645141cb
Ali Schlereth <aschlere@sbcglobal.net>
2017-01-06 10:50:42 -0700

    First user story passing

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,11 +1,13 @@
 class SearchController < ApplicationController
 
   def index
-    response = Faraday.get("https://api.bestbuy.com/v1/stores(area(80202,25))?format=json&show=storeId,storeType,longName,city,distance,phone&pageSize=15&apiKey=8zjkc9r7xng9wvqqjayyeya9")
-    stores_info = JSON.parse(response.body, symbolize_names: true)[:stores]
+    response = Faraday.get("https://api.bestbuy.com/v1/stores(area(80202,25))?format=json&show=storeId,storeType,longName,city,distance,phone&pageSize=15&apiKey=#{ENV["BEST_BUY_KEY"]}")
+    info = JSON.parse(response.body, symbolize_names: true)
+    stores_info = info[:stores]
     @stores = stores_info.map do |store_info|
       Store.new(store_info)
     end
+    @counts = [info[:total], info[:to]]
   end
 
 end
