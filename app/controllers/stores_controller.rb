9cb9008d621b031ffd9f34466dd26ba9ba563083
Ali Schlereth <aschlere@sbcglobal.net>
2017-01-06 11:58:00 -0700

    Refactor search controller then take back refactor for readability

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,10 +1,6 @@
 class StoresController < ApplicationController
 
   def show
-    # hour_info = Store.get_store_by_id(params[:id])
-    # hours = hour_info[:stores][0][:hours]
-    # store_info = {address: params[:address], city: params[:city], longName: params[:name], region: params[:state], postalCode: params[:zip], storeId: params[:id], storeType: params[:store_type], hours: hours}
-    # @store = Store.new(store_info)
     @store = Store.get_store_by_id(params)
   end
 
