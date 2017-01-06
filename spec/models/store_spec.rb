7ee49409018b707ca14106926c0601c39428b792
Ali Schlereth <aschlere@sbcglobal.net>
2017-01-06 12:10:16 -0700

    Time's up

--- spec/models/store_spec.rb
+++ spec/models/store_spec.rb
@@ -14,4 +14,29 @@ describe "Store" do
       expect(stores_info[0][0]).to respond_to('phone')
     end
   end
+
+  context "get store by id" do
+    it "returns a store by a given params with id" do
+      params = {address: "3000 East First Ave",
+                 city: "Denver",
+                 name: "Best Buy Mobile - Cherry Creek Shopping Center",
+                 state: "CO",
+                 store_type: "Mobile",
+                 zip: "80206",
+                 action: "show",
+                 controller: "stores",
+                 id: "2740"}
+      store = Store.get_store_by_id(params)
+
+      expect(store).to be_a(Store)
+      expect(store).to respond_to('name')
+      expect(store).to respond_to('store_type')
+      expect(store).to respond_to('address')
+      expect(store).to respond_to('city')
+      expect(store).to respond_to('state')
+      expect(store).to respond_to('zip')
+      expect(store).to respond_to('hours')
+
+    end
+  end
 end
