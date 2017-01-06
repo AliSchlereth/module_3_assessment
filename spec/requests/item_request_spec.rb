b13bcb77cf89315f80317437e31070e95703774a
Ali Schlereth <aschlere@sbcglobal.net>
2017-01-06 09:25:00 -0700

    Item by id api route working spec passing

--- spec/requests/item_request_spec.rb
+++ spec/requests/item_request_spec.rb
@@ -1,10 +1,12 @@
 require "rails_helper"
 
 describe "item requests" do
+  before :each do
+    @item1 = Item.create(name: "Name", description: "description", image_url: "http://image.jpg")
+    @item2 = Item.create(name: "Name2", description: "description2", image_url: "http://image2.jpg")
+  end
   context "get all items" do
     it "returns all items" do
-      item1 = Item.create(name: "Name", description: "description", image_url: "http://image.jpg")
-      item2 = Item.create(name: "Name2", description: "description2", image_url: "http://image2.jpg")
 
       get "/api/v1/items"
 
@@ -20,4 +22,23 @@ describe "item requests" do
       expect(items[0]).to_not have_key('updated_at')
     end
   end
+
+  context "get one items" do
+    it "returns an item by id" do
+
+      get "/api/v1/items/#{@item1.id}"
+
+      item = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(item).to be_a(Hash)
+      expect(item).to have_key('id')
+      expect(item).to have_key('name')
+      expect(item).to have_key('description')
+      expect(item).to have_key('image_url')
+      expect(item).to_not have_key('created_at')
+      expect(item).to_not have_key('updated_at')
+    end
+  end
+
 end
