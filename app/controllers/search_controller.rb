6e253502e76db77dd0610db258433cd075bc060f
Ali Schlereth <aschlere@sbcglobal.net>
2017-01-06 11:40:10 -0700

    Refactor Search Controller index

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,13 +1,9 @@
 class SearchController < ApplicationController
 
   def index
-    response = Faraday.get("https://api.bestbuy.com/v1/stores(area(80202,25))?format=json&show=storeId,storeType,longName,city,distance,phone,address,region,postalCode&pageSize=15&apiKey=#{ENV["BEST_BUY_KEY"]}")
-    info = JSON.parse(response.body, symbolize_names: true)
-    stores_info = info[:stores]
-    @stores = stores_info.map do |store_info|
-      Store.new(store_info)
-    end
-    @counts = [info[:total], info[:to]]
+    stores_info = Store.get_stores_by_zip(params[:search])
+    @stores = stores_info[0]
+    @counts = stores_info[-1]
   end
 
 end
--- app/models/store.rb
+++ app/models/store.rb
@@ -24,5 +24,20 @@ class Store
     @hours = attributes[:hours]
   end
 
+  def self.get_stores_by_zip(zip)
+    response = Faraday.get("https://api.bestbuy.com/v1/stores(area(#{zip},25))?format=json&show=storeId,storeType,longName,city,distance,phone,address,region,postalCode&pageSize=15&apiKey=#{ENV["BEST_BUY_KEY"]}")
+    info = JSON.parse(response.body, symbolize_names: true)
+    stores_info = info[:stores]
+    stores = process_stores_information(stores_info)
+    counts = [info[:total], info[:to]]
+    [stores, counts]
+  end
+
+  def self.process_stores_information(stores_info)
+    stores_info.map do |store_info|
+      Store.new(store_info)
+    end
+  end
+
 
 end
